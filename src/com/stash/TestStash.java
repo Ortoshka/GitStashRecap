fa4cf6584c38c5463995e48b0bb8d1b19ada8df6
Ortok Unipresent <ortokunipresent@Ortoks-MBP.home>
2020-11-17 22:16:41 -0500

    second TestStash file modified

--- src/com/stash/TestStash.java
+++ src/com/stash/TestStash.java
@@ -1,5 +1,8 @@
 package com.stash;
 
 public class TestStash {
-
+public static void main(String[] args) {
+	System.out.println("modification of our code");
+	System.out.println("hello world");
+}
 }
