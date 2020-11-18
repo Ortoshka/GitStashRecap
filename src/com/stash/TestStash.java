60202388a7a31cecdaa1cb77e3e75530eded4ca5
Ortok Unipresent <ortokunipresent@Ortoks-MBP.home>
2020-11-17 22:21:01 -0500

    git stash pop added

--- src/com/stash/TestStash.java
+++ src/com/stash/TestStash.java
@@ -4,5 +4,7 @@ public class TestStash {
 public static void main(String[] args) {
 	System.out.println("modification of our code");
 	System.out.println("hello world");
+	
+	System.out.println("git stash pop");
 }
 }
