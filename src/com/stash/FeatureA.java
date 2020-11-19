fc1dc3d17f29226f48ae4cbc9e6e159731dbb94b
Ortok Unipresent <ortokunipresent@Ortoks-MBP.home>
2020-11-18 21:15:48 -0500

    featureA completed

--- src/com/stash/FeatureA.java
+++ src/com/stash/FeatureA.java
@@ -3,5 +3,9 @@ package com.stash;
 public class FeatureA {
 public static void main(String[] args) {
 	System.out.println("Tomorow never today");
+	
+	System.out.println("file modified");
+	
+	System.out.println("featureA is done");
 }
 }
